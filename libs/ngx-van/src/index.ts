0b004f9e54195193152848c1340089451c0fe2a4
Oto Dočkal <oto.dockal@gmail.com>
2023-01-21 18:34:00 +0100

    fix: change barrel file

--- libs/ngx-van/src/index.ts
+++ libs/ngx-van/src/index.ts
@@ -1,4 +1,9 @@
+import { NgxVanItemDirective } from './lib/ngx-van-item.directive';
+import { NgxVanTriggerForDirective } from './lib/ngx-van-trigger-for.directive';
+import { NgxVanComponent } from './lib/ngx-van.component';
+
 export * from './lib/ngx-van.component';
 export * from './lib/ngx-van-item.directive';
 export * from './lib/ngx-van-trigger-for.directive';
-export * from './lib/index';
+
+export const NgxVan = [NgxVanItemDirective, NgxVanTriggerForDirective, NgxVanComponent];
--- libs/ngx-van/src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import { NgxVanItemDirective } from './ngx-van-item.directive';
-import { NgxVanTriggerForDirective } from './ngx-van-trigger-for.directive';
-import { NgxVanComponent } from './ngx-van.component';
-
-export const NgxVan = [NgxVanItemDirective, NgxVanTriggerForDirective, NgxVanComponent];
