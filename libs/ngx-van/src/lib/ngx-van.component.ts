81749705cf8434e8911aa59bb68504b023e49416
Oto Dočkal <oto.dockal@gmail.com>
2023-07-18 21:03:53 +0200

    fix: add ngSkipHydration attribute in order to allow run cmp with turn on hydration
    TMP solution for now.

--- libs/ngx-van/src/lib/ngx-van.component.ts
+++ libs/ngx-van/src/lib/ngx-van.component.ts
@@ -24,6 +24,9 @@ import { NgxVanService } from './ngx-van.service';
     standalone: true,
     selector: 'ngx-van',
     exportAs: 'ngxVan',
+    host: {
+        ngSkipHydration: '',
+    },
     animations: [ngxVanAnimations],
     template: `
         <!-- MOBILE: lazy, instatiate on demand using toggleMobileMenu() -->
