853d2d6a4ae7bb1f7a01da89cb40360bd60d79ef
Oto Dočkal <oto.dockal@gmail.com>
2023-07-18 21:02:02 +0200

    feat: migrate to latest nx & angular

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,7 +1,6 @@
+import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-    providers: [provideAnimations()],
-});
+bootstrapApplication(AppComponent, appConfig);
