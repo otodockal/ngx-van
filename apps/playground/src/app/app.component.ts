cd8c5566c9b2e86278701bd8a97ac41f036bd235
Oto Dočkal <oto.dockal@gmail.com>
2025-01-20 20:12:08 +0100

    feat: ng19

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -4,13 +4,12 @@ import { NgxVan, NgxVanItem, NgxVanTriggerFor } from 'ngx-van';
 import { IconComponent } from './icon/icon.component';
 
 @Component({
-    standalone: true,
     selector: 'ngx-van-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     host: {
         class: 'layout',
     },
-    imports: [IconComponent, RouterModule, NgxVan, NgxVanItem, NgxVanTriggerFor],
+    imports: [IconComponent, RouterModule, NgxVan, NgxVanItem, NgxVanTriggerFor]
 })
 export class AppComponent {}
